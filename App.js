b2df620ed4f80391e8769cfd5e26694f3c17a4e6
AlisyaAfiz <23018913@myrp.edu.sg>
2024-11-01 14:40:09 +0800

    Updated Folders

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TextInput, Button, Alert, TouchableOpacity, ToastAndroid} from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 
-export default function App() {
+const InputBox = ({label, onChangeText})=> {
+    return (
+        <View>
+            <Text>{label}</Text>
+            <TextInput style={{borderWidth: 1}} onChangeText={onChangeText} />
+        </View>
+    );
+};
+
+const MyApp =() => {
+  // let pw='';
+  const [pw, setPw] = useState('');
+    const [name, setName] = useState('');
+    const [type, setType] = useState('');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={{ padding: 20, paddingTop: 50 }}>
+
+        {/*Exercise 1B*/}
+        {/*<Text>User type:</Text>*/}
+        {/*<RNPickerSelect onValueChange={(value) => console.log(value)} items={[*/}
+        {/*  { label: 'Admin', value: 'Admin' },*/}
+        {/*  { label: 'Guest', value: 'Guest' },*/}
+        {/*]}*/}
+        {/*/>*/}
+
+        {/*Exercise 1A*/}
+        {/*<Text>Password:</Text>*/}
+        {/*<TextInput style={{borderWidth: 1}}/>*/}
+
+        {/*Exercise 1C and 1D*/}
+        {/*<Button title="LOG IN" onPress={() => Alert.alert(`Another Thing!`)}/>*/}
+
+        {/*Exercise 1E*/}
+        {/*<TouchableOpacity onPress={() => Alert.alert("Welcome!")}>*/}
+        {/*  <Text>Submit Answers</Text>*/}
+        {/*</TouchableOpacity>*/}
+
+        {/*Exercise 1F*/}
+        {/*<TouchableOpacity onPress={() => ToastAndroid.show("Welcome!",*/}
+        {/*    ToastAndroid.SHORT)}>*/}
+        {/*  <Text>LOG IN</Text>*/}
+        {/*</TouchableOpacity>*/}
+
+        {/*Exercise 1G*/}
+          <RNPickerSelect
+              onValueChange={(value) => setType(value)}
+              items={[
+                  { label: 'Admin', value: 'Admin' }, { label:
+                          'Guest', value: 'Guest' },
+              ]}
+          />
+          <InputBox label="User Name:" onChangeText={(text) => setName(text)}/>
+          <InputBox label="Password:" onChangeText={(text) => setPw(text)}/>
+          <TouchableOpacity onPress={()=>
+              ToastAndroid.show('Welcome ' + type + ' ' + name, ToastAndroid.SHORT)}>
+              <Text>LOG IN</Text>
+          </TouchableOpacity>
+          <Text>{pw}</Text>
+
+      </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default MyApp;
